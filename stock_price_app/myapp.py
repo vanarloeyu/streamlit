4e8779b8559bc412b6d46441321b08e32e2e3bd4
vanarloeyu <vanarloeyu@gmail.com>
2021-02-09 22:13:59 +0800

    stock_price_app

---
+++ stock_price_app/myapp.py
@@ -0,0 +1,25 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Tesla Inc!
+
+
+""")
+
+tickerSymbol = 'TSLA'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='id', start='2010-5-31', end='2020-5-31')
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
